7d0d3380950eb40aaacc199a2b12e9573f7a729e
marti <marti@alejandro>
2026-07-24 21:06:08 -0600

    repositorio con variable

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -14,7 +14,43 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
-}
+        System.out.println("Hola mundo me llamo alejandro");
+        System.out.println("este sera 1er");
+        System.out.println("programa¡¡¡¡¡¡¡¡");
+        
+        
+        System.out.print("vamos camino a");
+        System.out.print("la clase de");
+        System.out.print(" progra¡¡");
+        
+        System.out.printf("tu apellido es %s\n","marquez");
+        System.out.println("");
+        
+        System.out.printf("%s tu apellido es\n","marquez");
+        System.out.println("");
+        
+        System.out.printf("tu apellido %s es\n","marquez");
+        
+        System.out.println("Hola \n este es \n un nuevo\n amanecer");
+        System.out.println("hola \t este es \t un nuevo\t amanecer");
+        // variables
+        int edad; //tipo entero
+        double peso; //tipo decimal
+        char inicial; //tipo carapter letras,digitos, simbolos especiales
+        String nombre; //tipo cadena 
+        
+        edad = 21;
+        peso = 60.5;
+        inicial = 'D';
+        nombre ="marquez";
+        
+        System.out.printf("Tengo %d anios \n",edad);
+        System.out.printf("mi peso ideal es %f \n",peso);
+        System.out.printf("dime %c por mi inicial",inicial);
+        System.out.printf("%s es mi nombre",nombre);
+        
+        
+        // TODO code application logic here
+    }//fin de main
+}//fin de Class
