88762a63471a053e618f2291657c28981f1ba52d
marti <marti@alejandro>
2026-07-29 21:14:44 -0600

    uso de condiciones

--- Seleccion de vehiculo/src/seleccion/de/vehiculo/SeleccionDeVehiculo.java
+++ Seleccion de vehiculo/src/seleccion/de/vehiculo/SeleccionDeVehiculo.java
@@ -20,6 +20,13 @@ import java.util.Scanner;
  *     b.double
  * 3.condiciones 
  *     a.tipo de condiciones 
+ *             i.   >   -> mayor
+ *             ii.  <   -> menor
+ *             iii. >=  -> mayor o igual
+ *             iv.  <=  -> menor o igual
+ *             v.   ==  -> igual (son dos signos iguales juntos)
+ *             vi.  !=  -> distinto
+
  *     b.tipos de comparacion
  */
 public class SeleccionDeVehiculo {
@@ -36,11 +43,12 @@ public class SeleccionDeVehiculo {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
         int cantidadLitros =0;
-        char respuestoTipo ='x';
+        char respuestoTipo ='@';
+        String respuestaCadena = "John Doe";
         
         
-        
-        System.out.println("Me dijeron que eres dueño de un vehiculo\"verde\"");
+         System.out.println("Me dijeron que eres duenio de un vehiculo "
+                +   "\"Verde\"");
             System.out.println("tienes que echarle biodiesel");
             System.out.printf("cuantos litros de agarra?");
             cantidadLitros = entrada.nextInt();
@@ -53,9 +61,20 @@ public class SeleccionDeVehiculo {
             }
             System.out.println("");
         
-            entrada.next();
+            entrada.nextLine();//Limpiar el Buffer
             System.out.println("tu vehiculo es Diesel verde? (x = si, y = no");
             respuestoTipo = entrada.nextLine().charAt(0);
+            /*
+            System.out.printf("\nRespuesta Dada: %c",respuestoTipo);
+        
+            System.out.println("\nTu vehiculo es Diesel Verde? (x = si, y = no)");
+            respuestoTipo = entrada.nextLine().charAt(1);
+            System.out.printf("Respuesta Dada: %c",respuestoTipo);
+        
+            System.out.println("\nTu vehiculo es Diesel Verde? (x = si, y = no)");
+            respuestoTipo = entrada.nextLine().charAt(2);
+            System.out.printf("Respuesta Dada: %c",respuestoTipo);
+            */
         
         if(respuestoTipo =='x'){
             //condicion verdadera
@@ -70,6 +89,28 @@ public class SeleccionDeVehiculo {
             System.out.println("calentamiento global");
                 }
             System.out.println("");
+        
+            System.out.println("Tu carro es energeticamente eficiente?");
+            respuestaCadena = entrada.nextLine();
+            
+              /*
+         System.out.printf("Respuesta Ingresada: %s",respuestaCadena);
+        
+        System.out.println("\nSeguro que es asi?");
+        respuestaCadena = entrada.nextLine().toUpperCase();
+        System.out.printf("Respuesta Ingresada: %s",respuestaCadena);
+        
+        System.out.println("\n100% Seguro?");
+        respuestaCadena = entrada.next().toLowerCase();
+        System.out.printf("Respuesta Ingresada: %s",respuestaCadena);
+        */
+               if(respuestaCadena.equals("si") ){
+            System.out.println("Excelente");
+            System.out.println("Amigable con el ambiente");
+        }
+        else{
+            System.out.println("Uyy....busca que se puede hacer");
+        }
         
         
           
