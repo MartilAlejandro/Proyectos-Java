00786dcaa85a08e878da682ba096b15b92b9311c
marti <marti@alejandro>
2026-08-07 23:26:46 -0600

    descuentos de la tienda

--- Clasificación de Descuentos en Tienda de Software Enunciado/src/clasificación/de/descuentos/en/tienda/de/software/enunciado/ClasificaciónDeDescuentosEnTiendaDeSoftwareEnunciado.java
+++ Clasificación de Descuentos en Tienda de Software Enunciado/src/clasificación/de/descuentos/en/tienda/de/software/enunciado/ClasificaciónDeDescuentosEnTiendaDeSoftwareEnunciado.java
@@ -18,34 +18,44 @@ public class ClasificaciónDeDescuentosEnTiendaDeSoftwareEnunciado {
     public static void main(String[] args) {
         // TODO code application logic here
     
+    // Inicialización del lector de datos por consola
     Scanner teclado = new Scanner(System.in);
 
 
+    // Declaración de variables
     int edad = 0;
     char membresia = '@';
     double precioa = 0;
     double descuento =0;
 
+    // Impresión del título del programa en la consola
     System.out.println("Clasificacion de Descuentos en Tienda de Software Enunciado");
 
+    //salto de linea
     System.out.println();
     System.out.println();
 
+    // Limpieza del búfer
     System.out.print("Escriba la edad del cliente: ");
     edad = teclado.nextInt();
 
+    //salto de linea
     System.out.println();
     teclado.nextLine();
 
+    // Lectura del estado de la membresía
     System.out.print("El cliente cuenta con la membresia premium(S/N): ");
     membresia = teclado.nextLine().toUpperCase().charAt(0);
 
+    //salto de linea
     System.out.println();
 
+    // Lectura del precio original del artículo
     System.out.print("Escriba el precio del Articulo: ");
     precioa = teclado.nextDouble();
 
 
+    // Evaluación de las condiciones para aplicar los descuentos correspondientes
   if(edad >= 18){
 
   if(edad >65 || membresia =='S'){
@@ -75,6 +85,7 @@ public class ClasificaciónDeDescuentosEnTiendaDeSoftwareEnunciado {
    
    }
 
+  // Despliegue de los resultados finales
    System.out.println("\n Informacion de la compra es");
    System.out.println("\n ---------------------------");
    System.out.printf("\n El descuento aplicado es: %.2f lps\n", descuento);
